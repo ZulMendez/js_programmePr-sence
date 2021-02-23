e8d5413d588b2e8c57a9a6eab26fdc2162670bf1
zulma <zumendez7@gmail.com>
2021-02-23 10:57:57 +0100

    ajout des stagiaires corrigé, fonction bouton delete

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,76 @@
-console.log(localStorage.length);
+console.log(localStorage);
 
 let divStag = document.querySelector('#listStagiaires');
 let listStag = document.querySelector('.listStag');
 
-let inpKey = document.querySelector('#inpKey');
-let inpValue = document.querySelector('#inpValue');
+let inpNom = document.querySelector('#inpNom');
+let inpAge = document.querySelector('#inpAge');
+let inpCoding = document.querySelector('#inpCoding')
 let ajouteStag = document.querySelector('.ajouteStag');
 
 let removeStag = document.querySelector('.removeStag');
 let refreshStag = document.querySelector('.refreshStag');
-
+let input = document.querySelectorAll('input');
 
 // ajouter
-ajouteStag.onclick =  function() {
-    const key = inpKey.value;
-    const value = inpValue.value;
-
-    if (key && value) {
-        localStorage.setItem(key, value);
-        location.reload();
+function add(a, b, c, key) {
+    let lesStag = {
+        nom: a,
+        age: b,
+        coding: c,
     }
-}; 
+    let myObj = JSON.stringify(lesStag);
+    localStorage.setItem(key, myObj);
+    let myObjend = JSON.parse(localStorage.getItem(key)) 
 
-for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
+    console.log(myObjend);
 
-    // Ma liste
     let list = document.createElement('li');
-    list.innerText = inpValue.value;
+    list.innerText = inpNom.value;
     list.classList.add('stagiaire');
     listStag.appendChild(list);
 
-    listStag.innerHTML += `${value}`;
     // bouton de check
-    let checkBtn = document.createElement('button');
+    let checkBtn = document.createElement('input');
     checkBtn.innerHTML = '<i class="fas fa-check"></i>';
+    checkBtn.setAttribute('type', 'checkbox')
     checkBtn.classList.add('check-btn');
-    listStag.appendChild(checkBtn);
+    list.appendChild(checkBtn);
+
     // bouton de supprimer
     let removeBtn = document.createElement('button');
     removeBtn.innerHTML = '<i class="fas fa-trash"></i> remove';
     removeBtn.classList.add('remove-btn');
-    listStag.appendChild(removeBtn);
-};
+    list.appendChild(removeBtn);
 
-// effacer
+    inpNom.value = '';
+    inpAge.value = '';
+    inpCoding.value = '';
+
+    removeBtn.addEventListener('click', (e) => {
+        for (let i = 0; i< localStorage.length; i++) {
+            let key = localStorage.key(i);
+            localStorage.removeItem(key);
+            let parentListe = e.target.parentElement;
+            parentListe.remove()
+        }
+    })
+}
+
+let compteur = 0;
+ajouteStag.addEventListener('click', (e) => {
+    e.preventDefault();
+    let nom = input[0].value
+    let age = input[1].value
+    let coding = input[2].value
+    compteur++
+    add(nom, age, coding, compteur)
+});
+
+// clear storage/refresh
 removeStag.onclick = function() {
-    localStorage.clear(key);
+    localStorage.clear();
+    location.reload()
 };
 
 
